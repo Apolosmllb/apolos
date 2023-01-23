4b38432a55f73efb250e7349804292b1d714e40f
Apolosmllb <minermllb@gmail.com>
2023-01-22 21:49:55 -0500

    Create Home.tsx

---
+++ src/components/Home.tsx
@@ -0,0 +1,16 @@
+import {HiArrowNarrowRight} from "react-icons/hi";
+
+
+
+export const Home = () =>{
+    return(
+        <div  className="w-full h-screen bg-[#FDFFFC]">
+            {/**Container**/}
+            <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+                <p>Hi, my name is</p>
+                <h1>Miner Lozano</h1>
+                <h2>I'm a full Stack developer</h2>
+            </div>
+        </div>
+    )
+}
